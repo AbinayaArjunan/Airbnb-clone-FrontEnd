1b646c76cca61a8341b9036b2fc3b55f3315458f
abinayaarjunan <abiharsha2000@gmail.com>
2023-09-05 12:48:27 +0530

    commit changes

--- src/services/axios.jsx
+++ src/services/axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const BASE_URL = 'http://localhost:3001/api/v1';
-
+//const BASE_URL = 'https://airbnbbackend-xnxi.onrender.com/api/v1';
 export const axiosPrivate = axios.create(
   {baseURL: BASE_URL,
   headers: {'Content-Type': 'application/json'},
